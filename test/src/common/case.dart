fba2d71f9bb12729684f6dbed9445fbf239a95c9
rahul-thirstydevs <thirstydevs.rahul@gmail.com>
2023-08-28 10:40:34 +0530

    Update Intel Package

--- test/src/common/case.dart
+++
@@ -1,7 +0,0 @@
-class Case {
-  dynamic inp;
-  dynamic out;
-  String? desc;
-
-  Case({required this.inp, required this.out, this.desc});
-}
--- test/src/common/widget_builder.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-MaterialApp buildTestWidget(Widget child) {
-  return MaterialApp(
-    home: Material(
-      child: child,
-    ),
-  );
-}
